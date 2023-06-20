997338eae4208f29a8561889689fe1d6c8c1b72c
Fatim zahrae ZYATA <fatimzahraezyata@gmail.com>
2023-06-20 13:42:09 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-
 /**
  * print_alphabet - print the alphabet
  * Return: void
@@ -9,9 +7,10 @@
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a' ; i <= 'z' ; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-}	
+}
