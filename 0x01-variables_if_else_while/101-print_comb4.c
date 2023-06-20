b5eca5e99b15c34a21a206f107d8bc079af19b95
Fatim zahrae ZYATA <fatimzahraezyata@gmail.com>
2023-06-20 05:46:10 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,9 +13,9 @@ int main(void)
 
         for(i = '0' ; i <= '9' ; i++)
         {
-                for(j = '0' ; j <= '9' ; j++)
+                for(j = i+1 ; j <= '9' ; j++)
                 {
-                    for(k = '0' ; k <= '9' ; k++)
+                    for(k = j+1 ; k <= '9' ; k++)
                     {
                         putchar(i);
                         putchar(j);
