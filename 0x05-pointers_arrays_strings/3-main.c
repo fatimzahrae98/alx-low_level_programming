ed38083199de0a47177d8cbf068696943fcb1a54
Fatim zahrae ZYATA <fatimzahraezyata@gmail.com>
2023-06-26 14:53:08 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string
- * @str: The string to print
- * Return: void
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-void _puts(char *str)
-
+int main(void)
 {
-	for (; *str != '\0'; str++)
-	{
-	_putchar(*str);
-	}
-	_putchar('\n');
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
 }
